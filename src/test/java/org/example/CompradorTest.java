7055a529964ec3f79135f6617ee6e086d5ba80e3
Constanza Cristinich <109244120+krsconnie@users.noreply.github.com>
2023-05-30 05:09:59 -0400

    Se agregan Test en CompradorTest y ExpendedorTest

--- src/test/java/org/example/CompradorTest.java
+++ src/test/java/org/example/CompradorTest.java
@@ -1,21 +1,51 @@
 package org.example;
-
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.example.Expendedor.ProductoTipo.COCA;//Se utiliza COCA como caso general
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CompradorTest {
-
+class CompradorTest{
+    private Comprador comprador;
+    public Moneda500 moneda500;
+    private Expendedor expendedor;
+    @BeforeEach
+    void setUp() {
+        expendedor = new Expendedor(3,COCA);
+    }
     @AfterEach
     void tearDown() {
     }
 
     @Test
-    void cuantoVuelto() {
+    @DisplayName("Test Moneda nula")
+    public void TestMonedaNula() throws PagoIncorrectoException {
+        assertThrows(PagoIncorrectoException.class, () -> {
+            comprador = new Comprador(moneda500, COCA, expendedor);
+            expendedor.comprarProducto(moneda500,COCA);
+        });
+    }
+    @Test
+    @DisplayName("Test Recibe Vuelto de un producto")
+    public void RecibirVuelto1() throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException {
+        Moneda1000 moneda1000 = new Moneda1000();
+        comprador = new Comprador(moneda1000, COCA, expendedor);
+        assertNotNull(expendedor.comprarProducto(moneda1000,COCA));
+        assertNotNull(expendedor.getVuelto());
     }
 
     @Test
-    void queConsumiste() {
+    @DisplayName("Test recibe Vuelto multiples compras")
+    public void RecibirVuelto2() throws PagoIncorrectoException, NoHayProductoException, PagoInsuficienteException{
+        Moneda1000 moneda1000 = new Moneda1000();
+        comprador = new Comprador(moneda1000,COCA, expendedor);
+        assertNotNull(expendedor.comprarProducto(moneda1000,COCA));
+        assertNotNull(expendedor.getVuelto());
+        assertNotNull(expendedor.comprarProducto(moneda1000,COCA));
+        assertNotNull(expendedor.getVuelto());
     }
 }
--- src/test/java/org/example/ExpendedorTest.java
+++ src/test/java/org/example/ExpendedorTest.java
@@ -1,21 +1,57 @@
 package org.example;
-
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.example.Expendedor.ProductoTipo.COCA;//Se utiliza COCA como caso general
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExpendedorTest {
+    private Expendedor expendedor;
+    public Moneda500 moneda500;
 
+    @BeforeEach
+    void setUp() {
+        expendedor = new Expendedor(3, Expendedor.ProductoTipo.COCA);
+    }
     @AfterEach
     void tearDown() {
     }
-
     @Test
-    void comprarProducto() {
+    @DisplayName("Test 3 compras exitosas")
+    public void TestTresComprasExitosas() throws Exception{
+        moneda500 = new Moneda500();
+        assertNotNull(expendedor.comprarProducto(moneda500,COCA));
+        assertNotNull(expendedor.comprarProducto(moneda500,COCA));
+        assertNotNull(expendedor.comprarProducto(moneda500,COCA));
     }
-
     @Test
-    void getVuelto() {
+    @DisplayName("Test No hay Producto excepcion")
+    public void TestCompraConDepositoVacio() throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        moneda500 = new Moneda500();
+        assertThrows(NoHayProductoException.class, ()->{
+        expendedor.comprarProducto(moneda500,COCA);
+        expendedor.comprarProducto(moneda500,COCA);
+        expendedor.comprarProducto(moneda500,COCA);
+        expendedor.comprarProducto(moneda500,COCA);
+    });
+    }
+    @Test
+    @DisplayName("Test Pago Incorrecto")
+    public void TestPagoIncorrecto()throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
+        assertThrows(PagoIncorrectoException.class, () -> {
+            expendedor.comprarProducto(moneda500, COCA);//moneda500 = null
+        });
+    }
+    @Test
+    @DisplayName("Test Pago Insuficiente")
+    public void TestPagoInsuficiente() throws NoHayProductoException, PagoInsuficienteException,PagoIncorrectoException {
+        assertThrows(PagoInsuficienteException.class, () -> {
+            Moneda100 moneda100 = new Moneda100();
+            expendedor.comprarProducto(moneda100, COCA);
+        });
     }
 }
