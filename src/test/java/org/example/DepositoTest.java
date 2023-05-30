b96c32bcd6297b9dabd2b33e6a2d2ea04a81bb03
Constanza Cristinich <109244120+krsconnie@users.noreply.github.com>
2023-05-30 17:06:01 -0400

    Se agrega Test Deposito

--- src/test/java/org/example/DepositoTest.java
+++ src/test/java/org/example/DepositoTest.java
@@ -1,21 +1,47 @@
 package org.example;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.example.Expendedor.ProductoTipo.COCA;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-class DepositoTest {
+public class DepositoTest {
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    @DisplayName("Test getItem cuando hay elementos")
+    public void testGetItemCuandoHayElementos() {
+        Deposito<Moneda> deposito = new Deposito<>();
+        deposito.addItem(new Moneda100());
+        deposito.addItem(new Moneda500());
+
+        Moneda moneda1 = deposito.getItem();
+        Moneda moneda2 = deposito.getItem();
+
+        assertEquals(Moneda100.class, moneda1.getClass());
+        assertEquals(Moneda500.class, moneda2.getClass());
     }
 
     @Test
-    void getItem() {
+    @DisplayName("Test getItem cuando no hay elementos")
+    public void testGetItemCuandoNoHayElementos() {
+        Deposito<Producto> deposito = new Deposito<>();
+
+        Producto producto = deposito.getItem();
+
+        assertNull(producto);
     }
 
     @Test
-    void addItem() {
+    @DisplayName("Test addItem")
+    public void testAddItem() {
+        Deposito<Producto> deposito = new Deposito<>();
+        CocaCola coca = new CocaCola(1);
+        deposito.addItem(coca);
+
+        Producto producto = deposito.getItem();
+
+        assertEquals(CocaCola.class, producto.getClass());
     }
 }
